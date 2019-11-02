8f357ea92d65fe1a187804634ed0695fa7b4d426
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-02 17:58:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,44 +1,32 @@
-class CashRegister
-  attr_accessor :discount, :total
-  
 
-  def initialize(discount = 0)
+def initialize(discount = 0)
     @total = 0
     @discount = discount
-    @cart = []
+    @items = []
+    @last_transaction = []
   end
 
-  def add_item(item, price, quantity = 1)
-    item_info = {}
-    item_info[:name] = item
-    item_info[:price] = price
-    item_info[:quantity] = quantity
-
-    @cart << item_info
-
+  def add_item(item_name, price, quantity = 1)
     @total += price * quantity
-
+    quantity.times { @items.push(item_name) }
+    @last_transaction.push([item_name, price, quantity])
   end
 
   def apply_discount
-    if @discount == 0
-      return "There is no discount to apply."
-    end
-    @total -= @total * @discount / 100
-    return "After the discount, the total comes to $#{@total}."
-  end 
+    if discount == 0
+       "There is no discount to apply."
+     else
+       @total -= (@total * (@discount / 100.0)).to_i
+       "After the discount, the total comes to $#{@total}."
+     end
+   end
 
-  def items
-    item_names = []
-    @cart.each do | item_info |
-      for qty in 1..item_info[:quantity] 
-        item_names << item_info[:name]
-      end 
-    end 
-    item_names
-  end 
-  
-  def void_last_transaction
-    self.total = @total - @transactions.pop
-  end
-end 
+   def items
+     @items
+   end
+
+   def void_last_transaction
+     last_transaction = @last_transaction.pop
+     @total -= last_transaction[1] * last_transaction[2]
+   end
+end
